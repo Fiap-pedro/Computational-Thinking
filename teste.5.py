618c66e5eab4cf72c4cf4e11b0e6c5be51dd097f
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-03-22 20:41:22 -0300

    teste 5

---
+++ teste.5.py
@@ -0,0 +1,6 @@
+#programa que conta quantas letras tem o nome da pessoa e qual a primeira letra do nome
+
+nome = input('Olá, qual seu nome?')
+print(f'seja bem vindo {nome}')
+
+print(f'{nome}, seu nome tem {len(nome)} letras e a primeira letra do seu nome é {nome[0]}' )
