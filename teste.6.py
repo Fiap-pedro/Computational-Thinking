3cff3b0f323c1aaa25f8b2d0baf25ccacdfa9093
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-03-22 20:58:44 -0300

    testes 6 e 7

---
+++ teste.6.py
@@ -0,0 +1,2 @@
+nome = input('Ola, qual seu nome?')
+print(f'Seja bem vindo, {nome}')
---
+++ teste.7.py
@@ -0,0 +1,3 @@
+nome = input('Ola, qual seu nome?')
+idade = int(input('E qual sua idade?'))
+print(f'Seja bem vindo {nome}, feliz em saber que tem {idade} anos')
