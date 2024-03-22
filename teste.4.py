efaa3e056a8040784982bb512fb27de249c362bc
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-03-22 20:11:15 -0300

    teste 4

--- teste.4.py
+++ teste.4.py
@@ -1,2 +1,14 @@
-print('teste')
-print("olá")
+print('EMPRÉSTIMO')
+print('Para solicitar seu empréstimo precisaremos de alguns dados para validar...')
+nome = input('Qual seu nome?')
+idade = int(input('Qual sua idade?'))
+salario = int(input('Quanto você recebe?'))
+secret = int(input('senha secreta:'))
+
+#flag = senha secreta, válida tudo independente da idade e sálario#
+
+s = idade >= 18
+n = salario >= 1700
+flag = 1
+
+print(s and n or flag == secret)
