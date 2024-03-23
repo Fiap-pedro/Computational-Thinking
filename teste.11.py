b4239e38f60df445ed65faf96b8a733a8c211dad
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-03-22 22:53:42 -0300

    fim de aula

---
+++ teste.11.py
@@ -0,0 +1,8 @@
+a = int(input('digite um número:'))
+b = int(input('digite outro número:'))
+
+if a > b:
+    print(f'{a} é maior')
+
+else:
+    print(f'{b} é maior')
---
+++ teste.12.py
@@ -0,0 +1,17 @@
+n1 = ('digite um número:')
+
+if n1 % 2 == 0:
+if n1 < 100:
+    print(f'{n1} é par e menor que cem')
+
+if n1 % 2 == 0:
+if n1 >= 100:
+    print(f'{n1} é par e maior/igual que cem')
+
+if n1 % 2 != 0:
+if n1 < 100:
+    print(f'{n1} é ímpar e menor que cem')
+
+if n1 % 2 != 0:
+if n1 >= 100:
+    print(f'{n1} é ímpar e maior/igual a 100')
