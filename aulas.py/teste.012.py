1315935caedc8ca0c3f8cfb814904656d877963b
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-04-05 20:01:18 -0300

    Update teste.012.py

--- aulas.py/teste.012.py
+++ aulas.py/teste.012.py
@@ -5,13 +5,17 @@ if n1 % 2 == 0:
         print(f'{n1} é par e menor que cem')
 
 if n1 % 2 == 0:
-    if n1 >= 100:
-        print(f'{n1} é par e maior/igual que cem')
+    if n1 > 100:
+        print(f'{n1} é par e maior que cem')
 
 if n1 % 2 != 0:
     if n1 < 100:
         print(f'{n1} é ímpar e menor que cem')
 
 if n1 % 2 != 0:
-    if n1 >= 100:
-        print(f'{n1} é ímpar e maior/igual a 100')
+    if n1 > 100:
+        print(f'{n1} é ímpar e maior que 100')
+
+elif n1 == 100:
+    print(f'{n1} é par e igual á 100')
+print('__FÉ__')
