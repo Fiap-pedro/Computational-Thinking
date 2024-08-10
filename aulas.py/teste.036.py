55f8d26133d57bd3a338c5f8c63cfce92424d46b
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-08-09 21:23:45 -0300

    a

--- aulas.py/teste.036.py
+++ aulas.py/teste.036.py
@@ -1,2 +1,13 @@
+n = int(input('LINHA: '))
+estrelas = n * 2 - 1
+count = 0
+for i in range(n):
+    print(f'{' '*(n-i-1)}{'*'*(2*i+1)}')
+
+
+for i in range(n):
+    print(f'{' ' * count}{'*' * estrelas}')
+    estrelas -= 2
+    count += 1
 
 
