c2ad5e1fb2a3de36024a7f9eedff2321c9ffc243
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-11-08 19:59:52 -0300

    a

--- recursão/teste1.py
+++ recursão/teste1.py
@@ -1,10 +1,3 @@
-n = 5
-fac = 1
-while n != 1:
-    fac *= n
-    n -= 1
-
-
 # para criar uma recursão, tem que achar o caso base da função (0! = 1 ==> if n == 0: return 1) logo em seguida coloca a formula desejada usando a formula para que ela se repita
 def fatorial(n):
     if n == 0:
